7998877e8936d86aa6ae14f62f95ea566a881a9f
Pavan Kumar Pannala <pavankumar.pannala@gmail.com>
2021-11-13 08:42:10 +0530

    Add project files.

---
+++ BinarySearchTree/Node.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class Node
+    {
+      
+        public int Value { get; set; }
+
+        public Node left { get; set; }
+
+        public Node right { get; set; }
+
+        public Node(int val)
+        {
+            Value = val;
+            left = null;
+            right = null;
+
+        }
+
+    }
+}
