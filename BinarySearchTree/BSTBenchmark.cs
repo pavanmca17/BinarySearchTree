d6bca5987282a95c299482556f142702985d59c1
Pavan Kumar Pannala <pavankumar.pannala@gmail.com>
2021-11-21 15:03:33 +0530

    added benchmarking

---
+++ BinarySearchTree/BSTBenchmark.cs
@@ -0,0 +1,28 @@
+﻿using BenchmarkDotNet.Attributes;
+using BinarySearchTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class BSTBenchmark
+{
+   
+    private static readonly Tree treeProcess = new Tree();
+
+    [Benchmark]
+    public void Benchmark()
+    {
+        int startNumber = 10;
+        int endNumber = 20;
+        Random rnd = new Random();
+        List<int> numbers = Enumerable.Range(startNumber, endNumber).OrderBy(_ => rnd.Next()).ToList();
+                
+        for (int i = 0; i < numbers.Count(); i++)
+        {
+            treeProcess.Add(numbers[i]);
+        }
+
+        var root = treeProcess.ReturnRoot();
+        treeProcess.Inorder(root);
+    }
+}
--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BenchmarkDotNet.Running;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -8,24 +9,38 @@ namespace BinarySearchTree
     {
         static void Main(string[] args)
         {
+            //RunBSTBenchmark();
+            CreateandRunBST();
+            Console.ReadKey();
+        }
+
+        private static void RunBSTBenchmark()
+        {
+            var summary = BenchmarkRunner.Run<BSTBenchmark>();
+        }
+
+        private static void CreateandRunBST()
+        {
             Console.WriteLine("Binary Search Tree");
             Console.WriteLine("Please Enter the Starting Offset for random number genertation");
-            int startNumber = int.Parse(Console.ReadLine());           
+            int startNumber = int.Parse(Console.ReadLine());
             Console.WriteLine("Please Enter the Number of elements(Range) for random number genertation");
-            int endNumber = int.Parse(Console.ReadLine());        
+            int endNumber = int.Parse(Console.ReadLine());
             Random rnd = new Random();
             List<int> numbers = Enumerable.Range(startNumber, endNumber).OrderBy(_ => rnd.Next()).ToList();
-          
+
+
+            // TimeSpan timeSpan = new TimeSpan();
 
             Tree tree = new Tree();
             for (int i = 0; i < numbers.Count(); i++)
             {
-                tree.Add(numbers[i]);              
+                tree.Add(numbers[i]);
             }
 
             // get refernece to the root node
             var root = tree.ReturnRoot();
-            
+
             //Call to In-Order
             Console.WriteLine("InOrder");
             tree.Inorder(root);
@@ -42,7 +57,7 @@ namespace BinarySearchTree
             Console.WriteLine("Postorder");
             tree.Postorder(root);
 
-          
+
             Console.ReadKey();
         }
     }
